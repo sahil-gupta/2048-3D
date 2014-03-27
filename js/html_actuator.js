01356c5405b2ec86e5a71e9164449ff705302f71
Michal Opler <joppi07@gmail.com>
2014-03-27 17:54:16 +0100

    large tiles, see #5

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -61,6 +61,9 @@ HTMLActuator.prototype.addTile = function (tile) {
 
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
+
+  if (tile.value > 2048) classes.push("tile-super");
+
   this.applyClasses(element, classes);
 
   element.textContent = tile.value;
